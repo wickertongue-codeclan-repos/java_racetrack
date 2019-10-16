1417b0ae87aae020f46b648f3b67c29c6ed5d414
hayden <haydenwb@gmail.com>
2019-10-16 14:56:46 +0100

    a car now has wheels and engine, all tests passing

---
+++ src/main/java/components/SetOfWheels.java
@@ -0,0 +1,8 @@
+package components;
+
+public class SetOfWheels extends Component {
+
+    public SetOfWheels(String make, String model) {
+        super(make, model);
+    }
+}
---
+++ src/test/java/components/SetOfWheelsTest.java
@@ -0,0 +1,27 @@
+package components;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SetOfWheelsTest {
+
+    SetOfWheels setOfWheels;
+
+    @Before
+    public void before() {
+        setOfWheels = new SetOfWheels("Michelin", "DE23");
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Michelin", setOfWheels.getMake());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("DE23", setOfWheels.getModel());
+    }
+    
+}
